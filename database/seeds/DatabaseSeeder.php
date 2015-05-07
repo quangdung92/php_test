150d02b0fb782b3653492cc337c17287e7e9883c
QuangDung <quangdung.hvbc@gmail.com>
2015-05-07 17:31:55 +0700

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class DatabaseSeeder extends Seeder {
 
@@ -12,9 +13,23 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
+		$this->call('UserTableSeeder');
+		 
+        $this->command->info('User table seeded!');
 
 		// $this->call('UserTableSeeder');
 	}
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+		User::create([
+			'name' => 'sa1234',
+			'email' => 'sa1234@gmail.com',
+			'password' => 'sa12345'
+		]);
+    }
 
 }
